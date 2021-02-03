da75d9e558253fbe8a47c90a6357e2fe1697c1d8
César Alves <alvescesar@hotmail.com>
2021-02-03 15:56:11 -0300

    First ESlint

--- models/connection.js
+++ models/connection.js
@@ -7,9 +7,9 @@ async function connection() {
   if (schema) return Promise.resolve(schema);
   return MongoClient
     .connect(process.env.DB_URL || 'mongodb://localhost:27017/webchat', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
     .then((conn) => conn.db(process.env.DB_NAME))
     .then((dbSchema) => {
       schema = dbSchema;
