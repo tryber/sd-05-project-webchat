6d014d869607d250752b02500642a13453f114b9
hleoc <informig@gmail.com>
2021-02-04 16:14:07 -0300

    Requisito 1 - Trocando local da views de public para a pasta views

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'views')));
 
-app.set('views', path.join(__dirname, 'public'));
+app.set('views', path.join(__dirname, 'views'));
 
 app.engine('html', require('ejs').renderFile);
 
