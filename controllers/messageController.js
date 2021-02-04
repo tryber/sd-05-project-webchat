e780f5fe17fb7a0d4be3eeb6b3ce3d4b7e56e444
Felipe Vieira <flpvieira86@gmail.com>
2021-02-04 08:21:14 -0300

    PARA TUDO

--- controllers/messageController.js
+++
@@ -1,8 +0,0 @@
-const { getMessages } = require('../models/messages');
-
-module.exports = {
-  messagesList: async (_, res) => {
-    const messages = await getMessages();
-    res.status(200).render('index', { messages });
-  },
-};
--- server.js
+++ server.js
@@ -10,33 +10,76 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
-const { addMessage } = require('./models/messages');
-const { messagesList } = require('./controllers/messageController');
+const { addMessage, getMessages } = require('./models/messages');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'public'));
 
-app.engine('html', require('ejs').renderFile);
+// app.engine('html', require('ejs').renderFile);
 
 app.set('view engine', 'ejs');
 
-app.get('/', messagesList);
+let onlineUsers = [];
+
+app.get('/', async (_, res) => {
+  const messages = await getMessages();
+  res.status(200).render('index', { messages, onlineUsers });
+});
 
 io.on('connection', async (socket) => {
-  console.log(`Usuário conectado! ID: ${socket.id}`);
+  /*
 
-  socket.emit('logedIn', socket.id);
+      sockets
 
+  */
+
+  /*
+      usuário logou
+  */
+
+  const userId = socket.id;
+  console.log(`Usuário conectado! ID: ${userId}`);
+  const randomNick = `User${parseInt(Math.random() * 100000, 10)}`;
+  onlineUsers.unshift({ userId, nickname: randomNick });
+  io.emit('userLogedIn', randomNick, userId);
+  io.emit('anotherUserChangedNick', randomNick, socket.id);
+
+  /*
+      usuário deslogou
+  */
+  socket.on('disconnect', () => {
+    console.log(`Usuário desconectado! ID: ${userId}`);
+    onlineUsers = onlineUsers.filter((user) => user.userId !== userId);
+    io.emit('quit', userId);
+    socket.disconnect(0);
+  });
+
+  /*
+      usuário enviou msg
+  */
   socket.on('message', async ({ nickname, chatMessage }) => {
     const dateTime = new Date().getTime();
     const date = moment(dateTime).format('DD-MM-yyyy h:mm:ss A');
     await addMessage({ nickname, chatMessage, date });
     const message = `${date} - ${nickname}: ${chatMessage}`;
-    socket.broadcast.emit('message', message);
+    console.log(message);
+    io.emit('message', message);
   });
 
-  socket.on('disconnect', () => {
-    console.log(`Usuário desconectado! ID: ${socket.id}`);
+  /*
+      usuário trocou nick
+  */
+
+  socket.on('nickChange', (nickname) => {
+    onlineUsers = onlineUsers.map((user) => {
+      if (user.userId === userId) {
+        const u = user;
+        u.nickname = nickname;
+        return u;
+      }
+      return user;
+    });
+    io.emit('anotherUserChangedNick', nickname, userId);
   });
 });
 
