12e3bf170b8c467e7baa0b396a7674e64ad20203
Rafael Assad <rafaassadeng@gmail.com>
2021-02-06 10:55:38 -0300

    lint

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
+
 const app = express();
 const cors = require('cors');
 const server = require('http').createServer(app);
