e34df4f15df254744cd8c12b06e7fca3893b7026
Douglas <douglaza@gmail.com>
2021-02-10 19:04:57 -0300

    All requirements done

--- server.js
+++ server.js
@@ -24,38 +24,35 @@ app.set('views', path.join(__dirname, 'views'));
 // Arquivos estáticos dentro da pasta views
 app.use(express.static(path.join(__dirname, 'views')));
 
+let onlineUsers = [];
+
 app.get('/', async (req, res) => {
   const historyMessage = await messagesModel.getAll();
+  console.log(onlineUsers);
   res.render('index', { historyMessage, onlineUsers });
 });
-
-let onlineUsers = [];
 
 io.on('connection', (socket) => {
   const fake = faker.name.firstName();
   onlineUsers.push({ id: socket.id, nickname: fake });
-  // console.log(`${socket.id} now connected`);
-  console.log(onlineUsers);
-  
-  // socket.emit('connection', socket.id, fake);
+  console.log(`${socket.id} now connected`);
 
-  io.emit('users updated', {id: socket.id, onlineUsers} );
+  io.emit('users updated', { id: socket.id, onlineUsers });
 
   socket.emit('welcome', `Welcome to server, ${fake}!!`);
 
   socket.on('change nickname', (nickname) => {
-    const userIndex = onlineUsers.findIndex( user => user.id === socket.id );
-    onlineUsers[userIndex] = { id: socket.id, nickname}
-    console.log(nickname, ' changed');
-    console.log(onlineUsers)
-    io.emit('users updated', {id: socket.id, onlineUsers});
+    const userIndex = onlineUsers.findIndex((user) => user.id === socket.id);
+    onlineUsers.slice(userIndex, 1);
+    onlineUsers.push({ id: socket.id, nickname });
+    io.emit('users updated', { id: socket.id, onlineUsers });
   });
 
   socket.on('disconnect', () => {
     console.log(`${socket.id}: disconnected`);
-    const updatedUsers = onlineUsers.filter( (user) => user.id !== socket.id );
+    const updatedUsers = onlineUsers.filter((user) => user.id !== socket.id);
     onlineUsers = updatedUsers;
-    io.emit('users updated', {id: socket.id, onlineUsers} );
+    io.emit('users updated', { id: socket.id, onlineUsers });
   });
 
   socket.on('message', async (message) => {
@@ -66,7 +63,6 @@ io.on('connection', (socket) => {
     await messagesModel.postMessage(post);
     const formattedPost = `${post.timestamp} - ${post.nickname || fake}: ${post.chatMessage}`;
     io.emit('message', formattedPost);
-    // socket.broadcast.emit('message', message);
   });
 
   socket.on('error', (error) => {
@@ -78,4 +74,4 @@ io.on('error', (error) => {
   console.log('Server error: ', error.message);
 });
 
-http.listen(PORT, () => console.log(`The Shinning is listening on ${PORT}!`));
+http.listen(PORT, () => console.log(`The Shining is listening on ${PORT}!`));
