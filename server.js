9dc73ae57e7d19cef67cd45c91bf01efa483dae4
Mau <mauricio.sdjr@gmail.com>
2021-02-15 15:01:01 -0300

    alguns requisitos refatorados para passar no teste de um usuario e eliminado quando desconectado

--- server.js
+++ server.js
@@ -20,14 +20,37 @@ app.use(cors());
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
+let usersOnline = [];
+
 app.get('/', async (req, res) => {
   const messages = await getAllMessages();
 
-  res.render(path.join(__dirname, './views/index.ejs'), { messages });
+  res.render(path.join(__dirname, './views/index.ejs'), { messages, usersOnline });
 });
 
 io.on('connection', (socket) => {
+  const socketId = socket.id;
+  const userNickname = `Guest${Math.round(Math.random() * 1000)}`
   console.log('A user connected.');
+
+  socket.on('updateNickname', (newNickname) => {
+    usersOnline = usersOnline.map((user) => {
+      if (user.socketId === socketId) {
+        const newUser = user;
+        newUser.nickname = newNickname;
+        return newUser;
+      }
+
+      return user;
+    });
+
+    io.emit('updateNickname', newNickname, socketId);
+    /* io.emit('usersList', newNickname, socketId); */
+  });
+
+  usersOnline.push({ nickname: userNickname, socketId: socket.id });
+  socket.emit('saveCurrentId', socket.id);
+  io.emit('usersList', userNickname, socketId);
 
   socket.on('message', async ({ nickname, chatMessage }) => {
     const timestamp = moment(new Date()).format('DD-MM-yyyy hh:mm:ss A');
@@ -38,6 +61,8 @@ io.on('connection', (socket) => {
 
   socket.on('disconnect', () => {
     console.log('A user disconnected.');
+    usersOnline = usersOnline.filter((user) => user.socketId !== socketId);
+    io.emit('userDisconnected', socketId);
   });
 });
 
