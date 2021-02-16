62e2ccd6cdb13e52b2c2cb99ad65d9c43db44d54
Andrevlc <andrevezi@gmail.com>
2021-02-16 19:27:59 -0300

    Req 1 working on tests

---
+++ models/messageModel.js
@@ -0,0 +1,23 @@
+const moment = require('moment');
+const getCollection = require('./getCollection');
+
+const formatDate = 'DD-MM-yyyy HH:mm:ss';
+
+const createMessage = async (nickname, chatMessage) => {
+  const con = await getCollection();
+  const createdMessage = await con.collection('messages').insertOne({
+    nickname,
+    chatMessage,
+    time: moment(Date.now()).format(formatDate),
+  });
+  return createdMessage.ops[0];
+};
+
+const getMessages = async () => {
+  const getM = await getCollection.collection('messages').find().sort({ time: 1 }).toArray();
+  return getM.map((message) => `${message.time} - ${message.nickname}: ${message.chatMessage}`);
+};
+module.exports = {
+  createMessage,
+  getMessages,
+};
---
+++ server.js
@@ -0,0 +1,43 @@
+require('dotenv').config();
+
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const cors = require('cors');
+
+const app = express();
+const server = http.createServer(app);
+const io = require('socket.io')(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+const messageModel = require('./models/messageModel');
+
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', './public');
+app.set('view engine', 'ejs');
+
+app.get('/', async (req, res) => {
+  const getM = await messageModel.getMessages();
+  return res.render('index.ejs', { getM });
+});
+
+io.on('connection', async (socket) => {
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('user disconnected', socket.id);
+  });
+
+  socket.on('message', async (message) => {
+    const createMessage = await messageModel.createMessage(message.nickname, message.chatMessage);
+    const formatedMessage = `${createMessage.time}- ${createMessage.nickname}: ${createMessage.chatMessage}`;
+    io.emit('message', formatedMessage);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Listening server at port:${PORT}`);
+});
