3c538f6cc8611b1a4be29361ebe9f00ce7ca3952
pauloricardoz <trybe.przt@gmail.com>
2021-02-18 16:55:31 -0300

    list of users

--- server.js
+++ server.js
@@ -24,43 +24,42 @@ const connection = require('./model/connection');
 
 app.use(cors());
 
-function* idGeneratior() {
-  let id = 0;
-  while (true) {
-    yield (id += 1);
-  }
-}
-const GiveId = idGeneratior();
+let users = [];
 
 app.use(cors());
 
 // DD-MM-yyyy HH:mm:ss ${message.nickname} ${message.chatMessage} FORMATO
 app.set('view engine', 'ejs');
 app.set('views', './views');
-app.get('/', async (req, res) => {
+
+app.get('/', async (_req, res) => {
   const allMessages = await connection().then((db) =>
     db.collection('messages').find().toArray());
-  console.log(allMessages);
-  res.status(200).render('index', { id: GiveId.next().value, allMessages });
+  res.status(200).render('index', { allMessages, allUsers: users });
 });
-io.on('connection', async (socket) => {
-  /* connection()
-    .then((db) => db.collection('messages').find().toArray())
-    .then((allMessages) => {
-      socket.emit('oldMessages', allMessages);
-    }); */
-  socket.on('message', ({ chatMessage, nickname }) => {
-    console.log(`aqui ===>${chatMessage} ${nickname}`);
 
+io.on('connection', async (socket) => {
+  // io.broadcast.emit('newUser', )
+
+  socket.on('newUser', ({ nickname }) => {
+    const { id } = socket;
+    users.push({ id, nickname });
+    io.emit('UpdateUsers', { users });
+  });
+  socket.on('message', ({ chatMessage, nickname }) => {
     const time = dateFormat(new Date(), 'dd-mm-yyyy hh:MM:ss TT');
     const storeDB = `${time} - ${nickname}: ${chatMessage}`;
     connection().then((db) => db.collection('messages').insertOne({ message: storeDB }));
     io.emit('message', storeDB);
   });
+  socket.on('nicknameChanged', ({ nickname }) => {
+    users.find((user) => user.id === socket.id).nickname = nickname;
+    io.emit('UpdateUsers', { users });
+  });
   socket.on('disconnect', () => {
-    console.log(
-      'Lembre-se de deixar tudo relacionado a "conexão socket" dentro do evento "connection"',
-    );
+    users = users.filter((e) => e.id !== socket.id);
+    io.emit('UpdateUsers', { users });
+    console.log(socket.id, 'foi desconectado');
   });
 });
 
