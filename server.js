ee1353c4064d12fe4faeb87e73f55276d91f46d9
Rafael Quinteiro Leite <rafaelqleite@gmail.com>
2021-02-20 16:59:26 -0300

    algumas refatoracoes

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const express = require('express');
 const app = require('express')();
 const http = require('http').Server(app);
 const cors = require('cors');
@@ -12,30 +13,17 @@ const io = require('socket.io')(http, {
 
 const chatHistoryModel = require('./model/chatHistoryModel.js');
 
-const PORT = parseInt(process.env.PORT, 10) || 3000;
+const dateFormated = require('./functions/dateFormated');
 
-const dateFormated = (dataDeCriacao) => {
-  let meridium;
-  if (dataDeCriacao.getHours() >= 12) {
-    meridium = 'PM';
-  } else {
-    meridium = 'AM';
-  }
-  let [month, date, year] = dataDeCriacao.toLocaleDateString('en-US').split('/');
-  month = (`00${month}`).slice(-2);
-  date = (`00${date}`).slice(-2);
-  year = `${year} `.trim();
-  const [hour, minute, second] = dataDeCriacao.toLocaleTimeString('en-US').split(/:| /);
-  return `${date}-${month}-${year} ${hour}:${minute}:${second} ${meridium}`;
-  //  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
-  //  https://pt.stackoverflow.com/questions/175662/fun%C3%A7%C3%A3o-javascript-que-complete-o-campo-com-zeros-%C3%A0-esquerda
-};
+const PORT = parseInt(process.env.PORT, 10) || 3000;
 
 app.use(cors());
 
 app.set('view engine', 'ejs');
 
 app.set('views', './views');
+
+app.use(express.static(`${__dirname}/public`)); // para funcionar o css
 
 let usersOnline = [];
 
@@ -78,3 +66,4 @@ http.listen(PORT, () => {
 
 //  https://www.youtube.com/watch?v=8Y6mWhcdSUM&t=282s - indicação via Telegram do @Dandrea
 //  ref2: https://socket.io/docs/v3/client-api/index.html
+//  https://stackoverflow.com/questions/18629327/adding-css-file-to-ejs
