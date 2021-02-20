80e2e2569644beb677355365e9d233c44e9a7345
Rafael Quinteiro Leite <rafaelqleite@gmail.com>
2021-02-20 12:49:28 -0300

    item 4

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
-const express = require('express');
 const app = require('express')();
 const http = require('http').Server(app);
 const cors = require('cors');
@@ -38,20 +37,39 @@ app.set('view engine', 'ejs');
 
 app.set('views', './views');
 
-app.use(express.static('./'));
+let usersOnline = [];
 
 app.get('/', async (req, res) => {
   const bd = await chatHistoryModel.getAll();
   const historico = bd.map(({ timestamp, nickname, chatMessage }) => `${timestamp} ${nickname}: ${chatMessage}`);
-  res.status(200).render('index.ejs', { historico });
+  res.status(200).render('index.ejs', { historico, usersOnline });
 });
 
 io.on('connection', (socket) => {
+  let nicknameEsLint = `NickNameInicial - ${usersOnline.length}`;
+  usersOnline.push(nicknameEsLint);
+
+  socket.emit('initialNickname', nicknameEsLint);
+
   socket.on('message', async ({ chatMessage, nickname }) => {
     const timestamp = dateFormated(new Date());
     await chatHistoryModel.create(nickname, chatMessage, timestamp);
     io.emit('message', `${timestamp} ${nickname}: ${chatMessage}`);
   });
+
+  socket.on('changeNickName', ({ oldNickname, nicknameAtual }) => {
+    usersOnline = usersOnline.filter((item) => item !== oldNickname);
+    usersOnline.push(nicknameAtual);
+    nicknameEsLint = nicknameAtual;
+    io.emit('listUsersOnline', usersOnline);
+  });
+
+  socket.on('disconnect', () => { // ref2
+    usersOnline = usersOnline.filter((item) => item !== nicknameEsLint);
+    io.emit('listUsersOnline', usersOnline);
+  });
+
+  io.emit('listUsersOnline', usersOnline);
 });
 
 http.listen(PORT, () => {
@@ -59,3 +77,4 @@ http.listen(PORT, () => {
 });
 
 //  https://www.youtube.com/watch?v=8Y6mWhcdSUM&t=282s - indicação via Telegram do @Dandrea
+//  ref2: https://socket.io/docs/v3/client-api/index.html
