360b4ce0148df4b4641bbccd22fdcc76345ab76a
AndersonAGodoy <anderson.a.godoy@gmail.com>
2021-02-21 13:31:12 -0300

    all requisits done

--- server.js
+++ server.js
@@ -1,25 +1,56 @@
-const { MongoClient } = require('mongodb');
-require('dotenv').config();
+const express = require('express');
 
-let schema = null;
+const PORT = 3000;
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 
-async function connection() {
-  if (schema) return Promise.resolve(schema);
-  return MongoClient.connect(
-    process.env.DB_URL || 'mongodb://localhost:27017/webchat',
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
-    .then((conn) => conn.db(process.env.DB_NAME))
-    .then((dbSchema) => {
-      schema = dbSchema;
-      return schema;
-    })
-    .catch((err) => {
-      console.error(err);
-      process.exit(1);
-    });
-}
-module.exports = connection;
+const cors = require('cors');
+const path = require('path');
+const moment = require('moment');
+const { newMessage, getAllMessages } = require('./model/Message');
+
+app.use(cors());
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use('/', express.static(path.join(__dirname, './views')));
+
+let usersOnline = [];
+
+app.get('/', async (_req, res) => {
+  const messages = await getAllMessages();
+  const formatMessage = messages.map((message) => `${message.time} - ${message.nickname}: ${message.chatMessage}`);
+  res.render('index', { formatMessage, usersOnline });
+});
+
+const dateFormat = 'DD-MM-yyyy HH:mm:ss';
+
+io.on('connection', (socket) => {
+  const nickName = `random User ${Math.ceil(Math.random() * 1000)}`;
+  const userId = socket.id;
+  socket.emit('user', nickName, userId);
+  usersOnline.push({ nickName, userId });
+  io.emit('connection', nickName, userId);
+  socket.on('message', async ({ chatMessage, nickname }) => {
+    const time = moment(Date.now()).format(dateFormat);
+    const messageFormat = `${time} - ${nickname}: ${chatMessage}`;
+    await newMessage({ chatMessage, time, nickname });
+    io.emit('message', messageFormat);
+  });
+
+  socket.on('changeNick', (nick, id) => {
+    usersOnline = usersOnline.filter((user) => user.userId !== id);
+    usersOnline.push({ nickName: nick, userId: id });
+    socket.broadcast.emit('changeNick', { nick, id });
+  });
+  socket.on('disconnect', () => {
+    usersOnline = usersOnline.filter((user) => user.userId !== userId);
+    io.emit('userDisconnected', userId);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`app ouvindo na porta ${PORT}`);
+});
