670d5fe6cf0819511d7540fde9480fa508351430
Natalia Macedo <nataliasilvamacedo@gmail.com>
2021-02-22 00:04:32 -0300

    Teste faker

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const path = require('path');
 const dateFormat = require('dateformat');
+// eslint-disable-next-line import/no-extraneous-dependencies
 const faker = require('faker');
 
 // Escopo retirado do conteúdo 32.3
