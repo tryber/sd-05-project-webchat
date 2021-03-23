f5eeeabc962bc4bbcf618f1c95ed0003f858f1d0
ederusena <eder.sena@live.com>
2021-03-23 20:09:14 -0300

    chore: Requisito 1

--- models/Messages.js
+++ models/Messages.js
@@ -1,19 +1,18 @@
 const connection = require('../tests/helpers/db');
 
-const addMessage = async (data) => {
-  const db = await connection();
-  const newMessage = await db.collection('messages').insertOne(data);
-  // console.log(newMessage, 'new message');
-  return newMessage.ops[0];
-};
-
-const getAllMessages = async () => {
-  const db = await connection();
-  const allMessages = await db.collection('messages').find().toArray();
-  return allMessages;
-};
-
 module.exports = {
-  addMessage,
-  getAllMessages,
+  addMessage: async (msgData) => {
+    const db = await connection();
+    const insertedMsg = await db
+      .collection('messages')
+      .insertOne(msgData);
+    return insertedMsg.ops[0];
+  },
+  allMessages: async () => {
+    const db = await connection();
+    const messages = await db
+      .collection('messages')
+      .find().toArray();
+    return messages;
+  },
 };
--- server.js
+++ server.js
@@ -1,58 +1,29 @@
-const express = require('express');
 const moment = require('moment');
-const path = require('path');
-
-const PORT = process.env.PORT || 3000;
-
-const app = express();
-
-app.use(express.static(path.join(__dirname, 'views')));
-
-app.set('views', path.join(__dirname, 'views'));
-
-app.engine('html', require('ejs').renderFile);
-
-app.set('view engine', 'ejs');
-
+const server = require('express');
+const app = server();
 const http = require('http').createServer(app);
-
 const cors = require('cors');
-
-app.use(cors()); // Permite recursos restritos na página web serem pedidos a domínio externo
-
-const client = require('socket.io')(http, {
+const io = require('socket.io')(http, {
   cors: {
-    origin: 'http://localhost:3000', // url aceita pelo cors
-    methods: ['GET', 'POST'], // Métodos aceitos pela url
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
   },
 });
 
-const create = require('./controllers/index');
-const { createMessage } = require('./models/Messages');
+const { addMessage } = require('./models/Messages');
 
-app.use('/', create);
+app.use(cors());
 
-// Foi criado um envento chamado connecton e a cada socket que for criado
-// será renderizado para o client o id e a string connected
-client.on('connection', async (socket) => {
-  console.log(`${socket.id} connected`);
+io.on('connection', (socket) => {
+  console.log(`Usuário entrou no chat: ID ${socket.id}`);
 
-  // cada msg do client será composta pelo momento q enviou a msg, o nickname e a msg
+  // Requisito 1
   socket.on('message', async ({ nickname, chatMessage }) => {
-    const date = new Date().getTime();
-    const timeStamp = moment(date).format('DD-MM-yyyy hh:mm:ss');
-    const message = `${timeStamp} - ${nickname}: ${chatMessage}`;
-    await createMessage(timeStamp, nickname, chatMessage);
-
-    // toda vez que o evento message for emitido vai renderizar a msg no padrão da const message
-    socket.broadcast.emit('message', message);
-  });
-
-  socket.on('disconnect', () => {
-    console.log(`${socket.id} disconnect`);
+    const date = moment(new Date().getTime()).format('DD-MM-YYYY hh:mm:ss A');
+    addMessage({ nickname, chatMessage, date });
+    const msg = `${date} - ${nickname}: ${chatMessage}`;
+    io.emit('message', msg);
   });
 });
 
-http.listen(PORT, () => {
-  console.log(`Servidor ouvindo na porta ${PORT}`);
-});
+http.listen(process.env.PORT || 3000);
