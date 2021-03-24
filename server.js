645cc09d7b76abfde6863d28a9f2da871479645d
rafaelpbar <rafael.coper@outlook.com>
2021-03-24 13:09:18 -0300

    Nada

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const app = exress();
+const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const path = require('path');
 const cors = require('cors');
 const moment = require('moment');
-const { saveMessages, getMessages } = require('./model/webChatModel');
+// const { saveMessages, getMessages } = require('./model/webChatModel');
 
 app.use(cors());
 app.use(express.json());
