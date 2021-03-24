5a9d9105e1ad8659f6be737d405cbbaa9b32216b
ederusena <eder.sena@live.com>
2021-03-24 07:16:43 -0300

    chore: aqui

--- models/Messages.js
+++ models/Messages.js
@@ -1,18 +1,28 @@
-const connection = require('../tests/helpers/db');
+const connection = require('./connection');
+
+const newMessage = async (data) => {
+  try {
+    const db = await connection();
+    const message = await db.collection('messages').insertOne(data);
+    return message.ops[0];
+  } catch (error) {
+    console.error(error.message);
+    return null;
+  }
+};
+
+const getMessages = async () => {
+  try {
+    const db = await connection();
+    const messages = await db.collection('messages').find({}).toArray();
+    return messages;
+  } catch (err) {
+    console.error(err.message);
+    return null;
+  }
+};
 
 module.exports = {
-  addMessage: async (msgData) => {
-    const db = await connection();
-    const insertedMsg = await db
-      .collection('messages')
-      .insertOne(msgData);
-    return insertedMsg.ops[0];
-  },
-  allMessages: async () => {
-    const db = await connection();
-    const messages = await db
-      .collection('messages')
-      .find().toArray();
-    return messages;
-  },
+  newMessage,
+  getMessages,
 };
--- server.js
+++ server.js
@@ -1,28 +1,70 @@
+// Back-end server side
+
+// DEPENDENCIES
+
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
 const moment = require('moment');
-const app = require('express');
-const http = require('http').createServer(app);
+require('dotenv').config();
+
+// IMPORTATIONS
+
+const app = express();
+app.use(bodyParser.json());
+
+// SET IO & CORS
+
+const server = require('http').createServer(app);
 const cors = require('cors');
-const io = require('socket.io')(http, {
-  cors: {
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST'],
-  },
-});
+const io = require('socket.io')(server);
 
-const { addMessage } = require('./models/Messages');
+const Messages = require('./models/Messages');
 
+app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
-io.on('connection', (socket) => {
-  console.log(`Usuário entrou no chat: ID ${socket.id}`);
+const onlineUsers = {};
 
-  // Requisito 1
-  socket.on('message', async ({ nickname, chatMessage }) => {
-    const date = moment(new Date().getTime()).format('DD-MM-YYYY hh:mm:ss A');
-    addMessage({ nickname, chatMessage, date });
-    const msg = `${date} - ${nickname}: ${chatMessage}`;
-    io.emit('message', msg);
+io.on('connection', async (socket) => {
+  console.log('user connected');
+  const messages = await Messages.getMessages();
+  io.to(socket.id).emit('showMessageHistory', messages, 'public');
+
+  socket.on('userConection', (currentUser) => {
+    onlineUsers[socket.id] = currentUser;
+    io.emit('showOnlineUsers', onlineUsers);
+  });
+  socket.on('changeNickname', (nickname) => {
+    onlineUsers[socket.id] = nickname;
+    io.emit('showOnlineUsers', onlineUsers);
+  });
+
+  socket.on('message', async ({ nickname, chatMessage, receiver }) => {
+    let msg = {
+      nickname,
+      message: chatMessage,
+      timestamp: moment(new Date()).format('DD-MM-yyyy hh:mm:ss'),
+    };
+    if (!receiver) {
+      const message = await Messages.newMessage(msg);
+      io.emit('message', `${message.timestamp} - ${message.nickname}: ${message.message}`, 'public');
+    } else {
+      msg = { ...msg, receiver };
+      io.to(socket.id)
+        .to(receiver)
+        .emit('message', `${msg.timestamp} (private) - ${msg.nickname}: ${msg.message}`, 'private');
+    }
+  });
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+    delete onlineUsers[socket.id];
+    io.emit('showOnlineUsers', onlineUsers);
   });
 });
 
-http.listen(process.env.PORT || 3000);
+// PORT LISTENER
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server listening to port ${PORT}`);
+});
