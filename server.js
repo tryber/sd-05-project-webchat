820486576cac3704ffd27d99d4f32781b8830a84
rafaelpbar <rafael.coper@outlook.com>
2021-03-24 03:32:15 -0300

    xxx

--- server.js
+++ server.js
@@ -1,36 +1,96 @@
-const express = require('express');
-const app = exress();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
-const path = require('path');
-const cors = require('cors');
-const moment = require('moment');
-const { saveMessages, getMessages } = require('./model/webChatModel');
+// Desenvolvimento em conjuto do Samu =  PR #32
+const express = require("express");
+const cors = require("cors");
 
-app.use(cors());
+const {
+  createMessage,
+  createPrivateMessage,
+  getMessages,
+} = require("./model/messagesModel");
+
+const app = express();
+
+// require socket.io e protocolo http
+const http = require("http");
+const socketIo = require("socket.io");
+
+// Wss para protocolo http
+const server = http.createServer(app);
+// socket.io para wss
+const io = socketIo(server);
+
+const path = require("path");
+const moment = require("moment");
+
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
-app.get('/', (_req, res) => {
-  res.sendFile('index.html')
+// rota app.use do diretorio public
+app.use("/", express.static(path.join(__dirname, "public")));
+
+const onlineUsers = {};
+
+// concectando socket
+io.on("connection", async (socket) => {
+  const messages = await getMessages();
+  io.to(socket.id).emit("displayHistory", messages, "public");
+
+  socket.on("userConnection", (currentUser) => {
+    onlineUsers[socket.id] = currentUser;
+    io.emit("displayUsers", onlineUsers);
+  });
+
+  socket.on("updateNick", (nickname) => {
+    onlineUsers[socket.id] = nickname;
+    io.emit("displayUsers", onlineUsers);
+  });
+
+  socket.on("disconnect", () => {
+    delete onlineUsers[socket.id];
+    io.emit("displayUsers", onlineUsers);
+  });
+
+  socket.on('message', async ({ nickname, chatMessage, addressee }) => {
+    let msg;
+    if (!addressee) {
+      msg = await createMessage({
+        nickname,
+        message: chatMessage,
+        timestamp: moment(new Date()).format("DD-MM-yyyy hh:mm:ss"),
+      });
+      io.emit(
+        "message",
+        `${msg.timestamp} - ${nickname}: ${chatMessage}`,
+        "public"
+      );
+    } else {
+      msg = await createPrivateMessage({
+        nickname,
+        message: chatMessage,
+        timestamp: moment(new Date()).format("DD-MM-yyyy hh:mm:ss"),
+        addressee,
+      });
+      // https://socket.io/docs/v3/rooms/
+      io.to(socket.id)
+        .to(addressee)
+        .emit(
+          "message",
+          `${msg.timestamp} (private) - ${nickname}: ${chatMessage}`,
+          "private"
+        );
+    }
+  });
 });
 
-io.on('connection', (socket) => {
-  console.log('made socket connection', socket.id);
+// ---------------------------------------------------------------------------------------------
+// Endpoint GET para mensagens
+// exemplo de ejs response.render('caminho', {objeto no ejs que quer chamar});
+// app.get('/', async (req, res) => {
+//   const getAllMessages = await getMessages();
+//   res.status(200).render('index', { getAllMessages });
+// });
+// ----------------------------------------------------------------------------------------------
 
-// Handle chat event
-socket.on('message', async (data) => {
-    console.log(data);
-    const realTime = moment(new Date()).format("DD MM YYYY hh:mm:ss");
-    const msgFormated = `${realTime} - ${data.nickname}: ${data.chatMessage}`
-    console.log('server L28', msgFormated);
-    await saveMessages(msgFormated);
-    io.emit('message', msgFormated);
-});
-
-// Handle typing event
-// socket.on('typing', function(data){
-//     socket.broadcast.emit('typing', data);
-
-server.listen(3000, () => console.log('Listening on port 3000'));
-});
+const PORT = 3000;
+// const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
