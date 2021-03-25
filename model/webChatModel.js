362990a71dc7fb15070d9d9190cd5b89f15dfc77
rafaelpbar <rafael.coper@outlook.com>
2021-03-24 21:51:12 -0300

    Ta fluindo

--- model/webChatModel.js
+++ model/webChatModel.js
@@ -1,10 +1,10 @@
 const connection = require('./connection');
 
 async function saveMessages(message) {
-  const { realTime, chatMessage, nickname } = message;
+  const { realTime, nickname, chatMessage } = message;
   const db = await connection();
   const messageSaved = await db.collection('messages').insertOne({ realTime, chatMessage, nickname });
-
+  console.log('linha 7', messageSaved.ops[0]);
   return messageSaved.ops[0];
 };
 
--- server.js
+++ server.js
@@ -1,36 +1,47 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
-const path = require('path');
-const cors = require('cors');
-const moment = require('moment');
-// const { saveMessages, getMessages } = require('./model/webChatModel');
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+const path = require("path");
+const cors = require("cors");
+const moment = require("moment");
+const { saveMessages, getMessages } = require("./model/webChatModel");
+const faker = require('faker');
 
 app.use(cors());
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+app.set("views", "public");
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (_req, res) => {
-  res.sendFile('index.html')
+app.get("/", async (_req, res) => {
+  const historyMessages = await getMessages();
+  console.log("server L18", historyMessages);
+  res.render("index", { historyMessages });
 });
 
-io.on('connection', (socket) => {
-  console.log('made socket connection', socket.id);
 
-// Handle chat event
-socket.on('message', async (data) => {
+io.on("connection", (socket) => {
+  console.log("made socket connection", socket.id);
+  const randomName = faker.name.findName();
+  socket.emit('joinRoom', randomName);
+  socket.emit("history");
+
+  // Handle chat event
+  socket.on("message", async (data) => {
     console.log(data);
+    const { chatMessage, nickname } = data;
     const realTime = moment(new Date()).format("DD MM YYYY hh:mm:ss");
-    const msgFormated = `${realTime} - ${data.nickname}: ${data.chatMessage}`
-    console.log('server L28', msgFormated);
-    await saveMessages(msgFormated);
-    io.emit('message', msgFormated);
+    const msgFormated = `${realTime} - ${nickname}: ${chatMessage}`;
+    console.log("server L28", msgFormated);
+    await saveMessages({ realTime, nickname, chatMessage });
+    io.emit("message", msgFormated);
+  });
+
+  // Handle typing event
+  socket.on("typing", function (data) {
+    socket.broadcast.emit("typing", data);
+  });
 });
 
-// Handle typing event
-// socket.on('typing', function(data){
-//     socket.broadcast.emit('typing', data);
-
-server.listen(3000, () => console.log('Listening on port 3000'));
-});
+server.listen(3000, () => console.log("Listening on port 3000"));
