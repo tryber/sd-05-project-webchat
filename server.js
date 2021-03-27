a7168b51bdae10cfe66c4a6f8a39d643f67e6715
rafaelpbar <rafael.coper@outlook.com>
2021-03-27 00:45:35 -0300

    Thats it for tonight

--- server.js
+++ server.js
@@ -1,26 +1,36 @@
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
-const io = require('socket.io')(server);
+const io = require('socket.io')(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
 const path = require('path');
 const cors = require('cors');
 const moment = require('moment');
 const faker = require('faker');
-const { saveMessages, getMessages } = require('./model/webChatModel');
+const { saveMessages } = require('./model/webChatModel');
 
-app.use(cors());
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  })
+);
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.set('views', 'public');
 app.use(express.json());
 
-// aqui aplico no ejs mandando os usuários e o chat
+// aqui envio pro ejs mandando os usuários e o chat
 app.get('/', (_req, res) => {
   // const allMessages = await getMessages();
   res.status(200).render('index', { message: 'Foi?' });
 });
 
-const usersOnLine = [];
+let usersOnLine = [];
 
 io.on('connection', (socket) => {
   console.log('Made socket connection', socket.id);
@@ -35,17 +45,17 @@ io.on('connection', (socket) => {
   io.emit('userConnected', userId, nickName); // send to all users that are connected
 
   socket.on('saveNickName', (nick, id) => {
-    usersOnLine = usersOnLine.filter((user) => user.userId === userId);
+    usersOnLine = usersOnLine.filter((user) => user.userId !== id);
     usersOnLine.push({ userId: id, nickName: nick });
     console.log(usersOnLine);
   });
 
   socket.emit('usersOnline', usersOnLine);
 
-  socket.emit('joinRoom', nickName);
+  // socket.emit('privateChat', nickName);
 
   // Handle chat event
-  socket.on('saveMessage', async (data) => {
+  socket.on('message', async (data) => {
     console.log(data);
     const { chatMessage, nickname } = data;
     const realTime = moment(new Date()).format('DD MM YYYY hh:mm:ss');
@@ -56,9 +66,9 @@ io.on('connection', (socket) => {
   });
 
   // Handle typing event
-  socket.on('typing', function (data) {
-    socket.broadcast.emit('typing', data);
-  });
+  // socket.on('typing', function (data) {
+  //   socket.broadcast.emit('typing', data);
+  // });
 
   socket.emit('disconect', () => console.log('User left the room...'));
 });
