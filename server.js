4785a9764721033908a103812f8ffadcc1ddc981
rafaelpbar <rafael.coper@outlook.com>
2021-03-27 22:52:18 -0300

    Working clear. Thanks to Zambs

--- server.js
+++ server.js
@@ -24,53 +24,61 @@ app.set('view engine', 'ejs');
 app.set('views', 'public');
 app.use(express.json());
 
+let usersOnLine = [];
+
 // aqui envio pro ejs mandando os usuários e o chat
 app.get('/', (_req, res) => {
+  const nickName = faker.name.findName();
   // const allMessages = await getMessages();
-  res.status(200).render('index', { message: 'Foi?' });
+  res.status(200).render('index', { nickName, usersOnLine });
 });
-
-let usersOnLine = [];
 
 io.on('connection', (socket) => {
   console.log('Made socket connection', socket.id);
 
-  const userId = socket.id;
-  const nickName = faker.name.findName();
-  const user = { userId, nickName };
-  usersOnLine.push(user);
+  let userId = socket.id;
+  let nickName;
 
-  socket.emit('conected', userId, nickName); // send to current user
-
-  io.emit('userConnected', userId, nickName); // send to all users that are connected
-
-  socket.on('saveNickName', (nick, id) => {
-    usersOnLine = usersOnLine.filter((user) => user.userId !== id);
-    usersOnLine.push({ userId: id, nickName: nick });
+  socket.on('connected', (nick) => {
+    userId = socket.id;
+    nickName = nick;
+    const user = { userId, nickName };
+    usersOnLine.push(user);
     console.log(usersOnLine);
   });
 
-  socket.emit('usersOnline', usersOnLine);
+  io.emit('userConnected', userId, nickName); // send to all users that are connected
 
-  // socket.emit('privateChat', nickName);
-
-  // Handle chat event
-  socket.on('message', async (data) => {
-    console.log(data);
-    const { chatMessage, nickname } = data;
-    const realTime = moment(new Date()).format('DD MM YYYY hh:mm:ss');
-    const msgFormated = `${realTime} - ${nickname}: ${chatMessage}`;
-    console.log('server L28', msgFormated);
-    await saveMessages({ realTime, nickname, chatMessage });
-    io.emit('message', msgFormated); // vou enviar para renderizar na página
+  socket.on('saveNickName', (nick) => {
+    const id = socket.id;
+    usersOnLine = usersOnLine.filter((user) => user.userId !== id);
+    usersOnLine.push({ userId: id, nickName: nick });
+    console.log('Line 51 - usersOnLine:', usersOnLine);
   });
 
-  // Handle typing event
-  // socket.on('typing', function (data) {
-  //   socket.broadcast.emit('typing', data);
-  // });
+  //   socket.emit('usersOnline', usersOnLine);
 
-  socket.emit('disconect', () => console.log('User left the room...'));
+  //   // socket.emit('privateChat', nickName);
+
+  //   // Handle chat event
+  //   socket.on('message', async (data) => {
+  //     console.log(data);
+  //     const { chatMessage, nickname } = data;
+  //     const realTime = moment(new Date()).format('DD MM YYYY hh:mm:ss');
+  //     const msgFormated = `${realTime} - ${nickname}: ${chatMessage}`;
+  //     console.log('server L28', msgFormated);
+  //     await saveMessages({ realTime, nickname, chatMessage });
+  //     io.emit('message', msgFormated); // vou enviar para renderizar na página
+  //   });
+
+  //   // Handle typing event
+  //   // socket.on('typing', function (data) {
+  //   //   socket.broadcast.emit('typing', data);
+  //   // });
+
+  socket.on('disconnect', () => {
+    usersOnLine = usersOnLine.filter((user) => user.userId !== socket.id);
+  });
 });
 
 server.listen(3000, () => console.log('Listening on port 3000'));
