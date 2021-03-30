7526cf3cfe5d726575c3176aab540dd9cb50fb19
samueap <samueap@gmail.com>
2021-03-30 01:45:14 -0300

    local pass, Thanks to the Best problem solver! Sir Zambs

--- models/messagesModel.js
+++ models/messagesModel.js
@@ -1,20 +1,38 @@
 const connection = require('./connection');
 
-async function createMessage(chatMessage) {
+async function createMessage(chatMessage, addresse, target) {
   const db = await connection();
-  const message = await db.collection('messages').insertOne({ chatMessage });
+  const message = await db.collection('messages').insertOne({ chatMessage, addresse, target });
 
   return message.ops[0];
 }
 
+async function getPrivateMessages(some, other) {
+  const db = await connection();
+  const allMessagesPrivate = db
+    .collection('messages')
+    .find({
+      $or: [
+        { addresse: some, target: other },
+        { addresse: other, target: some },
+      ],
+    })
+    .toArray();
+  return allMessagesPrivate;
+}
+
 async function getMessages() {
   const db = await connection();
-  const allMessages = db.collection('messages').find({}).toArray();
+  const allMessages = db
+    .collection('messages')
+    .find({ addresse: { $eq: null } })
+    .toArray();
 
   return allMessages;
 }
 
 module.exports = {
   createMessage,
+  getPrivateMessages,
   getMessages,
 };
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // [Honestidade Acadêmica]
-// pair programming e ajuda Paulo Zambelli, eu(Samuel), Larissa e Sid
+// squad war programming Paulo Zambelli(Helper), eu(Samuel), Larissa e Sid
 const express = require('express');
 const bodyParser = require('body-parser');
 // Cross-Origin Resource Sharing
@@ -29,6 +29,7 @@ const io = socketIo(server, {
 });
 
 app.use(bodyParser.json());
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
 app.use(
   cors({
     origin: 'http://localhost:3000',
@@ -37,7 +38,7 @@ app.use(
 );
 
 // require models mensagens
-const { createMessage, getMessages } = require('./models/messagesModel');
+const { createMessage, getMessages, getPrivateMessages } = require('./models/messagesModel');
 
 // rota app.use do diretorio public
 app.use(express.static(path.join(__dirname, 'public')));
@@ -53,10 +54,10 @@ io.on('connection', (socket) => {
   console.log(`Socket conectado: ${socketId}`);
   // io.emit('conectado', `${socketId}`);
 
-  socket.on('newUserConectando', ({ myData }) => {
-    let dataUsers = myData.socketId;
-    dataUsers = socket.id;
-    onlineUsers[dataUsers.socketId] = dataUsers;
+  socket.on('newUserConectando', ({ myData: old }) => {
+    const myData = old;
+    myData.socketId = socket.id;
+    onlineUsers[myData.socketId] = myData;
     io.emit('updateUsers', { onlineUsers });
     // console.log(myData, 'AQUI ESTÁ O MYDATA');
   });
@@ -67,11 +68,17 @@ io.on('connection', (socket) => {
     console.log(`${socketId} está desconectado`);
   });
 
-  socket.on('message', ({ chatMessage, nickname }) => {
+  socket.on('message', ({ chatMessage, nickname, target = '' }) => {
     const data = moment(new Date()).format('DD-MM-yyyy hh:mm:ss');
+    // https://socket.io/docs/v3/rooms/
+    if (target !== '') {
+      const newMessage = `${data} (private) - ${nickname}: ${chatMessage}`;
+      createMessage(newMessage, socket.id, target);
+      return io.to(target).to(socket.id).emit('message', newMessage);
+    }
     const newMessage = `${data} - ${nickname}: ${chatMessage}`;
     createMessage(newMessage);
-    io.emit('message', newMessage);
+    return io.emit('message', newMessage);
   });
 
   socket.on('displayName', ({ myData }) => {
@@ -79,69 +86,30 @@ io.on('connection', (socket) => {
     io.emit('updateUsers', { onlineUsers });
     // console.log(onlineUsers);
   });
-
-  // MENSAGEM DENTRO DO MODEL getMessages
-  // const messages = await getMessages();
-  // Cria sala PUBLIC
-  // io.to(socket.id).emit('displayHistory', messages, 'public');
-
-  // USUARIO ATUAL = usar em mudar usuario
-  // socket.on('userConnection', (currentUser) => {
-  //   onlineUsers[socketId] = currentUser;
-  //   io.emit('displayUsers', onlineUsers);
-  // });
-
-  // NICKNAME
-  // socket.on('updateNick', (nickname) => {
-  //   onlineUsers[socketId] = nickname;
-  //   io.emit('displayUsers', onlineUsers);
-  // });
-
-  // Disconnect ANTIGO
-  // socket.on('disconnect', () => {
-  //   delete onlineUsers[socketId];
-  //   io.emit('displayUsers', onlineUsers);
-  // });
-
-  // CRIA CHAT MESSAGEM PUBLIC COM (nickname, chatMessage, addresse)
-
-  // socket.on('message', async ({ nickname, chatMessage, addressee }) => {
-  //   let msg;
-  //   if (!addressee) {
-  //     msg = await createMessage({
-  //       nickname,
-  //       message: chatMessage,
-  //       timestamp: moment(new Date()).format('DD-MM-yyyy hh:mm:ss'),
-  //     });
-  //     io.emit('message', `${msg.timestamp} - ${nickname}: ${chatMessage}`, 'public');
-  //   } else {
-  //     // CRIAR MENSAGEM PRIVATA
-  //     msg = await createPrivateMessage({
-  //       nickname,
-  //       message: chatMessage,
-  //       timestamp: moment(new Date()).format('DD-MM-yyyy hh:mm:ss'),
-  //       addressee,
-  //     });
-
-  // SALA PRIVATE
-
-  // https://socket.io/docs/v3/rooms/
-  // io.to(socketId)
-  //   .to(addressee)
-  //   .emit('message', `${msg.timestamp} (private) - ${nickname}: ${chatMessage}`, 'private');
-  // }
-  // });
 });
-
 // app.get('/', (_req, res) => {
 // const getAllMessages = [{ nome: 'BATE PAPO UOL', sala: 'sala: + 18' }];
 // return res.status(200).render('index', { getAllMessages, onlineUsers });
 
-let numeros = 0;
+let numbers = 0;
+// endpoint all messages, users, number by increment guest
 app.get('/', async (_req, res) => {
   const getAllMessages = await getMessages();
-  res.status(200).render('index', { getAllMessages, onlineUsers, numeros });
-  numeros += 1;
+  res.status(200).render('index', { getAllMessages, onlineUsers, numbers });
+  numbers += 1;
+});
+
+// endpoint para as messages private enderaçada by client
+app.get('/chat-private/:target/:addresse', async (req, res) => {
+  const { target, addresse } = req.params;
+  const getAllMessages = await getPrivateMessages(target, addresse);
+  res.status(200).json(getAllMessages);
+});
+
+// endpoint para as messages chatPublic
+app.get('/chat', async (_req, res) => {
+  const getAllMessages = await getMessages();
+  res.status(200).json(getAllMessages);
 });
 
 const PORT = 3000;
