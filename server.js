af083b2ffb800504374309421b5755842b4952bc
saturnian99 <saturnian.dsg@gmail.com>
2021-03-31 09:21:34 -0300

    Implementing online users and nickname handling events

--- server.js
+++ server.js
@@ -4,24 +4,60 @@ const http = require('http').createServer(app);
 const cors = require('cors');
 const io = require('socket.io')(http, {
   cors: {
-    origin: 'http//localhost:3000',
+    origin: 'http://localhost:3000',
     methods: ['GET', 'POST'],
   },
 });
+const {
+  uniqueNamesGenerator,
+  colors,
+  animals,
+} = require('unique-names-generator');
 
-const { addMessage } = require('./models/Message');
+const { addMessage, getAllMessages } = require('./models/Message');
 
 app.use(cors());
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+const onlineUsers = [];
+
+app.get('/', async (_req, res) => {
+  const messages = await getAllMessages();
+  res.status(200).render('index', { onlineUsers, messages });
+});
 
 io.on('connection', (socket) => {
-  console.log('User connected');
-  socket.on('disconnect', () => console.log('User disconnected'));
+  const userId = socket.id;
+  const tempNickname = uniqueNamesGenerator({
+    dictionaries: [colors, animals],
+    style: 'capital',
+    separator: '',
+  });
+
+  onlineUsers.unshift({ userId, nickname: tempNickname });
+  console.log(`User ${userId} - ${tempNickname} connected`);
+  socket.emit('connected', { userId, nickname: tempNickname });
+  socket.on('disconnect', () =>
+    console.log(`User ${userId} - ${tempNickname} disconnected`));
 
   socket.on('message', async ({ chatMessage, nickname }) => {
     const timestamp = moment().format('DD-MM-YYYY hh:mm:ss A');
     await addMessage({ chatMessage, nickname, timestamp });
     const msgString = `${timestamp} - ${nickname}: ${chatMessage}`;
     io.emit('message', msgString);
+  });
+
+  socket.on('nicknameChange', ({ nickname, userId: userId_ }) => {
+    console.log(nickname, userId);
+    const userIndex = onlineUsers.findIndex((user) => user.userId === userId_);
+    const user = onlineUsers[userIndex];
+    user.nickname = nickname;
+
+    console.log(
+      `User ${user.userId} change nickname from ${user.nickname} para ${nickname}.`,
+    );
+    io.emit('nicknameChanged', nickname, userId_);
   });
 });
 
